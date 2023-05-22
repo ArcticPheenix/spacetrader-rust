eda52c791a2340ddb2b87613123e037e05e858cf
ArcticPheenix <phoenix.ckelly@gmail.com>
2023-05-21 21:21:07 -0600

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ use reqwest::{ Client };
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     let config = config::read_config("config.toml")?;
-    println!("Parsed config: {:?}", config);
+    // println!("Parsed config: {:?}", config);
 
-    let result = get_agent(config.api.url, config.api.api_key).await?;
-    println!("Result: {:?}", result);
+    let result = get_agent(config.api.url, config.api.api_key).await;
+    match result {
+        Ok(data) => println!("Result: {:?}", data),
+        Err(e) if e.is_decode() => {
+            println!("Failed to deserialze the response: {}", e);
+        }
+        Err(e) => {
+            println!("Request failed: {}", e);
+        }
+    }
     
     Ok(())
 }
